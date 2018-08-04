be174f0c75812aec8819f7fcddad8101091176b6
Aldrin <augustine.clement101@gmail.com>
2018-08-04 12:29:58 -0500

    update

--- src/main.c
+++ src/main.c
@@ -1,149 +1,152 @@
-/* Hello World Example
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
-// #include "freertos/FreeRTOS.h"
-// #include "freertos/task.h"
-// #include "esp_system.h"
-// #include "esp_spi_flash.h"
-#include "rdkafka.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "bme680/bme680.h"
+#include "i2c.h"
+#define TASK_STACK_DEPTH 2048
+#define SPI_BUS       HSPI_HOST
+#define SPI_SCK_GPIO 22
+#define SPI_MOSI_GPIO 5
+#define SPI_MISO_GPIO 23
+#define SPI_CS_GPIO 2
 
-#include <stdio.h>
-#include <signal.h>
-#include <string.h>
-static int run = 1;
+#define SDA_PIN 18
+#define SCL_PIN 19
+//set i2c addresses to connect to 
+// #define MAX44009_ADDRESS1 0x4A
+// #define MAX44009_ADDRESS2 0x4B
 
-/**
- * @brief Signal termination of program
- */
-static void stop (int sig) {
-        run = 0;
-        fclose(stdin); /* abort fgets() */
+#define I2C_MASTER_ACK 0
+#define I2C_MASTER_NACK 1
+#define SSID             "Home"
+#define PASSWORD         "Casabl12"
+
+static bme680_sensor_t* sensor = 0;
+
+void user_task(void *pvParameters)
+{
+    bme680_values_float_t values;
+
+    TickType_t last_wakeup = xTaskGetTickCount();
+
+    // as long as sensor configuration isn't changed, duration is constant
+    uint32_t duration = bme680_get_measurement_duration(sensor);
+
+    while (1)
+    {
+        // trigger the sensor to start one TPHG measurement cycle
+        if (bme680_force_measurement (sensor))
+        {
+            // passive waiting until measurement results are available
+            vTaskDelay (duration);
+
+            // alternatively: busy waiting until measurement results are available
+            // while (bme680_is_measuring (sensor)) ;
+
+            // get the results and do something with them
+            if (bme680_get_results_float (sensor, &values))
+                printf("%.3f BME680 Sensor: %.2f °C, %.2f %%, %.2f hPa, %.2f Ohm\n",
+                       (double)sdk_system_get_time()*1e-3,
+                       values.temperature, values.humidity,
+                       values.pressure, values.gas_resistance);
+        }
+        // passive waiting until 1 second is over
+        vTaskDelayUntil(&last_wakeup, 1000 / portTICK_PERIOD_MS);
+    }
 }
-static void dr_msg_cb (rd_kafka_t *rk,
-                       const rd_kafka_message_t *rkmessage, void *opaque) {
-        if (rkmessage->err)
-                fprintf(stderr, "%% Message delivery failed: %s\n",
-                        rd_kafka_err2str(rkmessage->err));
-        else
-                fprintf(stderr,
-                        "%% Message delivered (%zd bytes, "
-                        "partition %"PRId32")\n",
-                        rkmessage->len, rkmessage->partition);
 
-        /* The rkmessage is destroyed automatically by librdkafka */
+void i2c_master_init()
+{
+	i2c_config_t i2c_config = {
+		.mode = I2C_MODE_MASTER,
+		.sda_io_num = SDA_PIN,
+		.scl_io_num = SCL_PIN,
+		.sda_pullup_en = GPIO_PULLUP_ENABLE,
+		.scl_pullup_en = GPIO_PULLUP_ENABLE,
+		.master.clk_speed = 400000
+	};
+	i2c_param_config(I2C_NUM_0, &i2c_config);
+	i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
 }
-
-
-
-
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+   return ESP_OK;
+}
+static void initialise_wifi(void)
+{
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    wifi_config_t sta_config = {
+        .sta = {
+            .ssid = "Home",
+            .password = "Casabl12",
+            .bssid_set = false
+        }
+    };
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+    ESP_ERROR_CHECK( esp_wifi_connect() );
+}
 void app_main()
 {
-    rd_kafka_t *rk;         /* Producer instance handle */
-    rd_kafka_topic_t *rkt;  /* Topic object */
-    rd_kafka_conf_t *conf;  /* Temporary configuration object */
-    char errstr[512];       /* librdkafka API error reporting buffer */
-    char buf[512];          /* Message value temporary buffer */
-    const char *brokers;    /* Argument: broker list */
-    const char *topic;      /* Argument: topic to produce to */
-    brokers = "10.0.1.29";
-    topic = "notify";
-    /*
-     * Create Kafka client configuration place-holder
-    */
-    conf = rd_kafka_conf_new();
-    /* Set bootstrap broker(s) as a comma-separated list of
-     * host or host:port (default port 9092).
-     * librdkafka will use the bootstrap brokers to acquire the full
-     * set of brokers from the cluster. */
-    if (rd_kafka_conf_set(conf, "bootstrap.servers", brokers,
-                          errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK) {
-            fprintf(stderr, "%s\n", errstr);
+    nvs_flash_init();
+    initialise_wifi();
+    uart_set_baud(0, 115200);
+    // Give the UART some time to settle
+    vTaskDelay(1);
+
+    // #ifdef SPI_USED
+
+    spi_bus_init (SPI_BUS, SPI_SCK_GPIO, SPI_MISO_GPIO, SPI_MOSI_GPIO);
+
+    // init the sensor connected to SPI_BUS with SPI_CS_GPIO as chip select.
+    sensor = bme680_init_sensor (SPI_BUS, 0, SPI_CS_GPIO);
+    
+    // #else  // I2C
+
+    // // Init all I2C bus interfaces at which BME680 sensors are connected
+    // i2c_init(I2C_BUS, I2C_SCL_PIN, I2C_SDA_PIN, I2C_FREQ);
+
+    // // init the sensor with slave address BME680_I2C_ADDRESS_2 connected to I2C_BUS.
+    // sensor = bme680_init_sensor (I2C_BUS, BME680_I2C_ADDRESS_2, 0);
+
+    // #endif  // SPI_USED
+
+ if (sensor)
+    {
+        /** -- SENSOR CONFIGURATION PART (optional) --- */
+
+        // Changes the oversampling rates to 4x oversampling for temperature
+        // and 2x oversampling for humidity. Pressure measurement is skipped.
+        bme680_set_oversampling_rates(sensor, osr_4x, osr_none, osr_2x);
+
+        // Change the IIR filter size for temperature and pressure to 7.
+        bme680_set_filter_size(sensor, iir_size_7);
+
+        // Change the heater profile 0 to 200 degree Celcius for 100 ms.
+        bme680_set_heater_profile (sensor, 0, 200, 100);
+        bme680_use_heater_profile (sensor, 0);
+
+        // Set ambient temperature to 10 degree Celsius
+        bme680_set_ambient_temperature (sensor, 10);
+            
+        /** -- TASK CREATION PART --- */
+
+        // must be done last to avoid concurrency situations with the sensor 
+        // configuration part
+
+        // Create a task that uses the sensor
+        xTaskCreate(user_task, "user_task", TASK_STACK_DEPTH, NULL, 2, NULL);
     }
-        /* Set the delivery report callback.
-         * This callback will be called once per message to inform
-         * the application if delivery succeeded or failed.
-         * See dr_msg_cb() above. */
-    rd_kafka_conf_set_dr_msg_cb(conf, dr_msg_cb);
-        /*
-         * Create producer instance.
-         *
-         * NOTE: rd_kafka_new() takes ownership of the conf object
-         *       and the application must not reference it again after
-         *       this call.
-         */
-    rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
-    if (!rk) {
-            fprintf(stderr,
-                "%% Failed to create new producer: %s\n", errstr);
-}
-/* Create topic object that will be reused for each message
-         * produced.
-         *
-         * Both the producer instance (rd_kafka_t) and topic objects (topic_t)
-         * are long-lived objects that should be reused as much as possible.
-         */
-        rkt = rd_kafka_topic_new(rk, topic, NULL);
-        if (!rkt) {
-                fprintf(stderr, "%% Failed to create topic object: %s\n",
-                        rd_kafka_err2str(rd_kafka_last_error()));
-                rd_kafka_destroy(rk);
-        }
-
-        /* Signal handler for clean shutdown */
-        signal(SIGINT, stop);
-
-        fprintf(stderr,
-                "%% Type some text and hit enter to produce message\n"
-                "%% Or just hit enter to only serve delivery reports\n"
-                "%% Press Ctrl-C or Ctrl-D to exit\n");
-
-        while (run && fgets(buf, sizeof(buf), stdin)) {
-                size_t len = strlen(buf);
-
-                if (buf[len-1] == '\n') /* Remove newline */
-                        buf[--len] = '\0';
-
-                if (len == 0) {
-                        /* Empty line: only serve delivery reports */
-                        rd_kafka_poll(rk, 0/*non-blocking */);
-                        continue;
-                }
-
-                /*
-                 * Send/Produce message.
-                 * This is an asynchronous call, on success it will only
-                 * enqueue the message on the internal producer queue.
-                 * The actual delivery attempts to the broker are handled
-                 * by background threads.
-                 * The previously registered delivery report callback
-                 * (dr_msg_cb) is used to signal back to the application
-                 * when the message has been delivered (or failed).
-                 */
-                if (rd_kafka_produce(
-                            /* Topic object */
-                            rkt,
-                            /* Use builtin partitioner to select partition*/
-                            RD_KAFKA_PARTITION_UA,
-                            /* Make a copy of the payload. */
-                            RD_KAFKA_MSG_F_COPY,
-                            /* Message payload (value) and length */
-                            buf, len,
-                            /* Optional key and its length */
-                            NULL, 0,
-                            /* Message opaque, provided in
-                             * delivery report callback as
-                             * msg_opaque. */
-                            NULL) == -1) {
-
-
-
-}
-
-
-
-        }
-}
+    else
+        printf("Could not initialize BME680 sensor\n");
+    }
